c832df1fecb502a46ac53aa7f1a2a93ecefa5fb5
Krunal Goswami <goswamikrunal8571865@gmail.com>
2026-05-03 12:25:29 +0530

    Handling the stop timer using ref

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+
 const TimerChallenge = ({ title, targetTime }) => {
     const [timerActive, setTimerActive] = useState(false);
     const [timerExpired, setTimerExpired] = useState(false);
+    const timer = useRef();
     const handleStartTimer = () => {
         setTimerExpired(false);
         setTimerActive(true);
-        setTimeout(() => {
+        timer.current = setTimeout(() => {
             setTimerExpired(true);
             setTimerActive(false);
         }, targetTime * 1000);
+    }
+
+    const handleStopTimer = () => {
+        clearTimeout(timer.current);
+        setTimerActive(false);
     }
   return (
     <section className="challenge">
@@ -16,7 +23,7 @@ const TimerChallenge = ({ title, targetTime }) => {
       {timerExpired && <p>You lost</p>}
       <p className="challenge-time">{targetTime} second{targetTime !== 1 ? 's' : ''}</p>
       <p>
-        <button onClick={handleStartTimer}>
+        <button onClick={timerActive ? handleStopTimer : handleStartTimer}>
             {timerActive ? 'Stop' : 'Start'} Challenge
         </button>
       </p>
