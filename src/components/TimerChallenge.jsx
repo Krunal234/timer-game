4ba6e9d34a51595aaac050ddc202b51b0c97b7b2
Krunal Goswami <goswamikrunal8571865@gmail.com>
2026-05-03 12:05:35 +0530

    Handle timer start logic

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 const TimerChallenge = ({ title, targetTime }) => {
+    const [timerActive, setTimerActive] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
+    const handleStartTimer = () => {
+        setTimerExpired(false);
+        setTimerActive(true);
+        setTimeout(() => {
+            setTimerExpired(true);
+            setTimerActive(false);
+        }, targetTime * 1000);
+    }
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost</p>}
       <p className="challenge-time">{targetTime} second{targetTime !== 1 ? 's' : ''}</p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={handleStartTimer}>
+            {timerActive ? 'Stop' : 'Start'} Challenge
+        </button>
       </p>
-      <p className="">
-        Times is running... / Timer is Inactive
+      <p className={timerActive ? 'active' : ''}>
+        {timerActive ? 'Timer is running...' : 'Timer is Inactive'}
       </p>
     </section>
   );
