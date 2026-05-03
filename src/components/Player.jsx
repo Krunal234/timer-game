06d4b978a7712897d773f125f3e7abef770f7ab3
Krunal Goswami <goswamikrunal8571865@gmail.com>
2026-05-03 11:22:21 +0530

    Handle player name input changes

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  const userInputHandler = (username) => {
+    setSubmitted(false);
+    setPlayerName(username);
+  }
+  const HandleSubmit = () => {
+    setSubmitted(true);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? playerName : "Guest"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={(e) => userInputHandler(e.target.value)} value={playerName} />
+        <button onClick={HandleSubmit}>Set Name</button>
       </p>
     </section>
   );
