64549b9562424e084473cade18ada74cc60bf9b8
Krunal Goswami <goswamikrunal8571865@gmail.com>
2026-05-03 21:44:44 +0530

    Update conditional result to show score

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -8,13 +8,16 @@ const ResultModal = ({ref, result, targetTime, remainingTime, onReset}) => {
         }
     }));
 
+    const userLost = remainingTime <= 0;
     const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - (remainingTime / (targetTime * 1000))) * 100);
 
     return (
        <dialog ref={dialogRef} className="result-modal">
-        <h2>You {remainingTime <= 0 ? 'lost' : 'won'}</h2>
+        {userLost ? <h2>You lost the game!</h2> : <h2>Your Score: {score}</h2>}
+
         <p>The target time was <strong>{targetTime}</strong> seconds.</p>
-        <p>You stopped the timer with <strong>{formattedRemainingTime}</strong> seconds remaining.</p>
+        {userLost ? <p>You didn't stop the timer in time!</p> : <p>You stopped the timer with <strong>{formattedRemainingTime}</strong> seconds remaining.</p>}
         <form method='dialog' onSubmit={onReset}>
           <button type='submit'>Close</button>
         </form>
